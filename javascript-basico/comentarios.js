107e7cf5ea98c233de1191edf3cdac26929e9cae
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-07-31 10:13:46 -0300

    comentários de código

---
+++ javascript-basico/comentarios.js
@@ -0,0 +1,16 @@
+// Comentário
+console.log("Olá mundo."); // Aqui tem outro comentário
+
+console.log("Linha 1");
+console.log("Linha 2");
+console.log("Linha 3");
+
+// vai ser ignorado
+// console.log("Linha 4");
+
+/*
+Comentário
+de
+várias
+linhas
+*/
