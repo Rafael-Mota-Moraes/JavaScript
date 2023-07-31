16bcd5a4147d94dadc8ea8f7d73f72ea413d8667
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-07-31 15:40:04 -0300

    Constantes com const

---
+++ javascript-basico/const.js
@@ -0,0 +1,15 @@
+const nome = "João";
+// nome = "Pedro";
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+const conta = primeiroNumero * segundoNumero;
+
+const contaTriplicada = conta * 3;
+
+// + - * /
+console.log(conta);
+console.log(contaTriplicada);
+
+console.log(typeof contaTriplicada);
