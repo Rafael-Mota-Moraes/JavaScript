d3610ce45261c23e58ba75d223aec7dd5c5f8e52
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-07-31 16:24:00 -0300

    ex2

---
+++ javascript-basico/exercecios/ex2.js
@@ -0,0 +1,9 @@
+const nome = "Rafael";
+const idade = 30;
+const peso = 84;
+const altura = 1.8;
+const imc = peso / (altura * altura);
+
+console.log(
+  `${nome} tem ${idade} anos, pesa ${peso} kg teem ${altura} de altura e seu imc é de ${imc}`
+);
