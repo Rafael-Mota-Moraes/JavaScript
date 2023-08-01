6dd7897b66681ebd64aa60f272eb2b4f69f164ee
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-08-01 12:21:26 -0300

    Operadores aritméticos, de atribuição e incremento

---
+++ javascript-basico/operadores.js
@@ -0,0 +1,34 @@
+/*
+  Aritméticos
+  + Adição / Concatenação
+  -
+  /
+  *
+  ** Potenciação
+  % Resto da divisão
+*/
+
+const num1 = 10;
+const num2 = 3;
+const num3 = "10";
+
+console.log(num1 + num2 + num3);
+console.log(num1 % num2);
+
+let contador = 1;
+contador++;
+contador++;
+++contador;
+--contador;
+// contador = contador + 1;
+// Incremento = ++
+// Decremento = --
+console.log(contador);
+
+contador += 10;
+console.log(contador);
+
+contador -= 5;
+console.log(contador);
+
+console.log("A" * 3); // NaN = Not a number
