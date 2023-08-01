f6345fe30dc107cc890e71769ee4c438176a4eb4
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-08-01 15:26:04 -0300

    Funções para strings

---
+++ javascript-basico/strings.js
@@ -0,0 +1,7 @@
+let umaString = 'Um "texto"';
+console.log(umaString);
+console.log(umaString.charAt(4));
+console.log(umaString.concat(" outro texto"));
+console.log(umaString.indexOf("texto"));
+console.log(umaString.replace("Um", "Outro"));
+console.log(umaString.split(" "));
