e0b225beb64801567e5976227723a1b3521f5c6b
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-08-02 16:54:55 -0300

    Objeto Math

---
+++ javascript-basico/math.js
@@ -0,0 +1,11 @@
+// Objeto Math
+
+let num1 = 9.585;
+
+console.log(Math.floor(num1));
+console.log(Math.ceil(num1));
+console.log(Math.round(num1));
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.random());
+console.log((Math.random() * 100).toFixed(0));
+console.log(Math.PI);
