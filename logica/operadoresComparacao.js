0a94e491a7e2f7a2c2eaa6e78c238c18341720db
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-08-18 09:51:46 -0300

    Operadores de comparação

---
+++ logica/operadoresComparacao.js
@@ -0,0 +1,20 @@
+/*
+Operadores de comparação
+
+> - maior que
+>= - maior ou igual a
+< - menor que
+<= - menor que ou igual a
+== - igualdade 
+=== - igualdade estrita
+!= - diferente
+!== - diferenca estrita
+*/
+
+console.log(10 > 5);
+console.log(10 >= 5);
+console.log(10 <= 11);
+console.log(11 == "11");
+console.log(11 === "11");
+console.log(11 != "11");
+console.log(11 !== "11");
