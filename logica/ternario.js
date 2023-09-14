82292e7007d5b77f454e08bcd63e5327246e3514
Rafael Mota Moraes <rafaelmota6622@gmail.com>
2023-09-14 09:25:56 -0300

    Operador ternário

---
+++ logica/ternario.js
@@ -0,0 +1,8 @@
+// ? :
+const pontuacaoUsuario = 999;
+
+pontuacaoUsuario > 100
+  ? console.log("Usuário VIP")
+  : console.log("Usuário normal");
+
+// (condição) ? 'Valor para verdadeiro' : 'Valor para falso'
