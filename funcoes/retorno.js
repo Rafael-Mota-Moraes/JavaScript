f9cde034cfecc2071015269435486ea5c7dcd1dc
Rafael Mota Moraes <rafaelmota6622@gmail.com>
2023-09-23 13:17:22 -0300

    retorno

---
+++ funcoes/retorno.js
@@ -0,0 +1,12 @@
+// return
+// Retorna um valor
+// Termina a função
+
+function soma(a, b) {
+  return a + b;
+
+  console.log("ISSO NÃO VAI EXECUTAR");
+}
+
+const cinco = soma(2, 3);
+console.log(cinco);
