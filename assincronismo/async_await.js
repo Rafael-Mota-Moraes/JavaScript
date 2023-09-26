9880aa11dd973689add19fa5b0a040ca6f342ce0
Rafael Mota Moraes <rafaelmota6622@gmail.com>
2023-09-26 15:29:14 -0300

    Async e await

---
+++ assincronismo/async_await.js
@@ -0,0 +1,35 @@
+function rand(min = 0, max = 3) {
+  min *= 1000;
+  max *= 1000;
+
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof msg != "string") {
+        reject("Caiu no erro!");
+        return;
+      }
+
+      resolve(msg.toUpperCase() + " - Passou na promise");
+      return;
+    }, tempo);
+  });
+}
+
+async function executa() {
+  try {
+    const fase1 = await esperaAi("fase 1", rand());
+    console.log(fase1);
+    const fase2 = await esperaAi("fase 2", rand());
+    console.log(fase2);
+    const fase3 = await esperaAi("fase 3", rand());
+    console.log(fase3);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+executa();
